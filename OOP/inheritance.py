33241fe7838d1aa545b5631217781a424f7e73c9
David-Daniel Ojebiyi <oladoja14@gmail.com>
2022-06-28 10:35:52 +0100

    Add progression.py

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -37,30 +37,3 @@ class PredatoryCreditCard(CreditCard):
       # if process balance, convert APR to monthly multiplicative factor
       monthly_factor = pow(self._apr, 1/12)
       self._balance *= monthly_factor
-
-class Progression:
-  """
-  Iterator producing a generic progression
-
-  Default Iteration returns numbers in projection 1, 2, 3, ...
-  """
-
-  def __init__(self, start=0):
-    """Initialize current to the first value of the progression."""
-    self._current = start
-
-  def _advance(self):
-    """
-    Update self._current to a new value on each iteration
-    
-    This is a protected method and can therefore be overridden in a
-    subclass to customize the progression.
-
-    Setting current to None designates the end of a finite progression
-
-    """
-    self._current += 1
-  
-  def __next__(self):
-    """Return the next element in iteration or raise StopIteration error"""
-
---
+++ OOP/progression.py
@@ -0,0 +1,83 @@
+class Progression:
+  """
+  Iterator producing a generic progression
+
+  Default Iteration returns numbers in projection 1, 2, 3, ...
+
+  Parameters:
+  start - int, indicating the beginning of the progression
+  """
+
+  def __init__(self, start=0):
+    """Initialize current to the first value of the progression."""
+    self._current = start
+
+  def _advance(self):
+    """
+    Update self._current to a new value on each iteration
+    
+    This is a protected method and can therefore be overridden in a
+    subclass to customize the progression.
+
+    Setting current to None designates the end of a finite progression
+
+    """
+    self._current += 1
+
+  def __next__(self):
+    """Return the next element in iteration or raise StopIteration error"""
+
+    if self._current is None:
+      raise StopIteration()
+    else:
+      answer = self._current
+      self._advance()
+      return answer
+
+  def __iter__(self):
+    """By convention, this method returns the class instance """
+    return self
+
+  def print_progression(self, n):
+    """Print next n values in the progression"""
+    print(' '.join(str(next(self)) for j in range(n)))
+
+
+class ArithmeticProgression(Progression):
+  """An Iterator producing an arithmetic progression"""
+
+  def __init__(self, increment=1, start=0):
+    """
+    Create a new arithmetic progression.
+
+    increment - Fixed constant to add to each term (default 1)
+    start     - Starting point of the iteration (default 0)
+    """
+
+    super().__init__(start)
+    self._increment = increment
+
+  def _advance(self):
+    """Update value by adding increment"""
+    self._current += self._increment
+
+    return self._current
+
+
+class GeometricProgression(Progression):
+  """An iterator producing a geometric progession"""
+
+  def __init__(self, base=2, start=0):
+    """
+      Create a new geometric progression
+
+      base    the fixed constant multiplied to each term per iteration (default 2)
+      start   the starting point of the iteration (default 0)
+    """
+    super().__init__(start)
+    self._base = base
+
+  def _advance(self):
+    self._current *= self._base
+
+    return self._current
